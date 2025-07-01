ac5a070ae8d7b2873c7c740cd4763a08cc99a650
Dilli Ganesh <64489401+DilliGanesh1@users.noreply.github.com>
2025-07-02 00:51:12 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyA03goPGHvIXjhYXXD8uFe57Frck4oQxCo",
+  authDomain: "bingo-70be4.firebaseapp.com",
+  databaseURL: "https://bingo-70be4-default-rtdb.firebaseio.com",
+  projectId: "bingo-70be4",
+  storageBucket: "bingo-70be4.firebasestorage.app",
+  messagingSenderId: "1051549977513",
+  appId: "1:1051549977513:web:95bf7b1812430920d392b8",
+  measurementId: "G-GZQ064K4P4"
+};
+
+firebase.initializeApp(firebaseConfig);
